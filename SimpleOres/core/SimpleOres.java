67511e530a276379d805e2e48defcc2a8a2e58a6
Zot201 <erju01@gmail.com>
2013-10-03 14:00:10 +0900

    Update SimpleOres.java

--- SimpleOres/core/SimpleOres.java
+++ SimpleOres/core/SimpleOres.java
@@ -156,6 +156,7 @@ public class SimpleOres
 		LanguageRegistry.instance().loadLocalization(new ResourceLocation("/assets/simpleores/langs/de_DE.xml").getResourcePath(), "de_DE", true);
 		LanguageRegistry.instance().loadLocalization(new ResourceLocation("/assets/simpleores/langs/ja_JP.xml").getResourcePath(), "ja_JP", true);
 		LanguageRegistry.instance().loadLocalization(new ResourceLocation("/assets/simpleores/langs/zh_TW.xml").getResourcePath(), "zh_TW", true);
+		LanguageRegistry.instance().loadLocalization(new ResourceLocation("/assets/simpleores/langs/zh_CN.xml").getResourcePath(), "zh_CN", true);
 		
 		/**
 		 * Registering things such as the world generator, tile entities and GUI's.
